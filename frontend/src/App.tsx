a8ec437935b0333c05281b10e9ffd47d894cbaa5
chihabMe <chihab.mg.me@gmail.com>
2022-07-22 00:17:25 +0200

    structuring the frontend files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {useState } from 'react';
 
 function App() {
-  const [name,setName]= useState<string|null>("");
-  setName(2);
   return (
     <div className="App">
       <header className="App-header">
